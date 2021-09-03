291411fad4c955628abb568c3707ee9a90a00d0b
Alek <alekvalencia2535@gmail.com>
2021-09-02 22:25:14 -0700

    added .message to err in catch on routes. added transactions from example to DB array to play with data. added a points removal controller. added a sort function to sort DB array by timestamp so returned array will be oldest first and newest last. next, I mapped that sorted array to get the objects to be in the format that I needed to return after removing points. next, instatiated a map object to be a dictionary. I then for looped through the mapped array to figure out which payers I would use to added up to the amount of points being taken. while the loop was doing that I also added functionality to add only one payer once if multiple and added the points up if there were multiple payers. I then used the forEach method on the map object to push new objects with the correct format to be returned and the amount of points taken from each payer.

--- controllers/rewardsCont.js
+++ controllers/rewardsCont.js
@@ -1,30 +1,107 @@
 const Rewards = require('../models/Rewards');
 
-let db = [];
+let db = [{ "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" }, { "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" }, { "payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z" }, { "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" }, { "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" }];
+let usedDB = [];
+let finalDB = [];
 
 const addReward = (payer, points) => {
     let newReward = new Rewards(payer, points);
     // console.log(newReward.getReward());
     db.push(newReward.getReward());
-    return newReward.getReward()
+    return newReward.getReward();
 
+}
+
+const pointRemoval = (points) => {
+    let total = points;
+    const sortedArr = db.sort((a, b) => {
+        if (a.timestamp < b.timestamp) {
+            return -1;
+        }
+        if (a.timestamp > b.timestamp) {
+            return 1;
+        }
+        return 0;
+    });
+
+    const mapArr = sortedArr.map(obj => {
+        let newObj = {};
+        newObj.payer = obj.payer;
+        newObj.points = obj.points;
+        return newObj;
+    });
+    
+    const checkMap = new Map();
+
+    for (let i = 0; i < mapArr.length; i++) {
+        // console.log(checkMap.has(mapArr[i].payer));
+
+        if (checkMap.has(mapArr[i].payer)) {
+            checkMap.set(mapArr[i].payer, checkMap.get(mapArr[i].payer) + mapArr[i].points);
+        } else {
+            checkMap.set(mapArr[i].payer, mapArr[i].points)
+        }
+
+        if (total > 0) {
+        total = total - mapArr[i].points;
+        // console.log(points); 
+        usedDB.push(mapArr[i]);
+        } 
+        if (total <= 0) {
+            break;
+        }
+    }
+    
+    let addedValues = 0;
+
+    checkMap.forEach((value, key) => {
+        let newObj = {};
+        let startPoint = points - value;
+        console.log(startPoint);
+        let newValue = startPoint - points;
+        console.log(newValue);
+        addedValues += value;
+        console.log(addedValues, '\n***BREAK***');
+
+        if (value > points) {
+            addedValues = addedValues - value;
+            startPoint = addedValues - points;
+            console.log(startPoint);
+            newValue = startPoint;
+            console.log(newValue);
+
+            newObj.payer = key;
+            newObj.points = newValue;
+            finalDB.push(newObj); 
+        } else {
+            // startPoint = points - value;
+            // console.log(startPoint);
+            // newValue = startPoint - points;
+            // console.log(newValue);
+            newObj.payer = key;
+            newObj.points = newValue;
+            finalDB.push(newObj);  
+        }
+    })
+
+    console.log(mapArr, usedDB, checkMap, finalDB);
 }
 
 const getRewards = () => {
-    let newObj = {}
+    console.log(db);
+    let newObj = {};
     db.forEach((reward) => {
-        newObj[reward.payer] = reward.points;
-    })
+        if (newObj.hasPropertyOf(reward.payer)) {
+          newObj[reward.payer] = reward.points;  
+        } else {
+            newObj[reward.payer] = newObj.points + reward.points;
+        }
+        
+    });
     return newObj;
-    // return db.map(reward => {
-    //     let newObj = {};
-    //     newObj[reward.payer] = reward.points;
-    //     return newObj;
-    // });
-}
 
-// addReward("Dove", 1000);
-// addReward("Nike", 5000);
+}
+pointRemoval(5000);
 
 module.exports = {
     addReward: addReward,
