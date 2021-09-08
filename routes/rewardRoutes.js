cab701a7e7ad431a7cd781bc5e1b022ea3f256a8
Alek <alekvalencia2535@gmail.com>
2021-09-07 18:28:25 -0700

    saving so I can merge.

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -7,7 +7,7 @@ router.post('/reward', (req, res) => {
       const pointObj = controllers.addReward(req.body.payer, req.body.points); 
       res.status(200).json(pointObj) 
     } catch (err) {
-        res.status(404).json(err)
+        res.status(404).json(err.message)
     }
     
 });
@@ -17,7 +17,7 @@ router.get('/reward', (req, res) => {
       const rewardsArr = controllers.getRewards(); 
       res.status(200).json(rewardsArr) 
     } catch (err) {
-        res.status(404).json(err)
+        res.status(404).json(err.message)
     }
     
 });
